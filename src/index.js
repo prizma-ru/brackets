03b23399f98ee4dd75e06e089f793543404126d5
prizma-ru <dmitry.glotov@gmail.com>
2021-08-21 20:39:18 +0300

    feat: add basic logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+ 
+    let stack = [];
+    const OPEN_BRACKETS = ['(', '{'];
+    const BRACKETS_PAIR = {
+      [')']: '(',
+      ['}']: '{',
+    };
+  
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+  
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+  
+        let topElement = stack[stack.length - 1];
+  
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  
 }
